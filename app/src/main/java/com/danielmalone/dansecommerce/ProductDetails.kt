f4fdf6917b518f3846796fdb1d2a91b925652aa5
User <user@Users-Air.lan>
2019-12-11 17:28:48 +0300

    Initial commit

---
+++ app/src/main/java/com/danielmalone/dansecommerce/ProductDetails.kt
@@ -0,0 +1,39 @@
+package com.danielmalone.dansecommerce
+
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.danielmalone.dansecommerce.repos.ProductsRepository
+import com.squareup.picasso.Picasso
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.product_details.*
+
+class ProductDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.product_details)
+
+        val title = intent.getStringExtra("title")
+
+        val product = ProductsRepository().getProductByName(title)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    product_name.text = it.title
+                    Picasso.get().load(it.photoUrl).into(photo)
+                    thePriceOfProduct.text = "$${it.price}"
+                }, {})
+
+        availability.setOnClickListener {
+            AlertDialog.Builder(this)
+                    .setMessage("Hey, $title is in stock!")
+                    .setPositiveButton("OK") { p0, p1 ->
+
+                    }
+                    .create()
+                    .show()
+        }
+    }
+}
